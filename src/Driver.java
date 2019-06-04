9b82f3918c2423bc831e5494d411076df34d069c
R Travis Pierce <artyp@ou.edu>
2019-06-04 02:54:28 -0500

    Added a lot of extra courses and ships and pilots and menu-ized selection of each

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
@@ -20,32 +21,196 @@ public class Driver {
 		Scanner keyboard = new Scanner(System.in);
 	
 		// Get name of track file name from user input
+		boolean trackSelected = false;
+		String trackSelection = "";
+		
 		System.out.println("Which track will we be racing today?");
-		String trackName = keyboard.nextLine();
-		Track raceTrack = new Track(trackName);
+		System.out.println("[Crash Canyon] || [Crag City] || [Big Rock Island] || [Outpost Pass]");
+		
+		while (!trackSelected)
+		{
+			String trackName = keyboard.nextLine();
+			if (trackName.equalsIgnoreCase("Crash Canyon"))
+			{
+				trackSelection = "CrashCanyon";
+				trackSelected = true;
+				break;
+			} else if (trackName.equalsIgnoreCase("Crag City"))
+			{
+				trackSelection = "CragCity";
+				trackSelected = true;
+				break;
+			} else if (trackName.equalsIgnoreCase("Big Rock Island"))
+			{
+				trackSelection = "BigRockIsland";
+				trackSelected = true;
+				break;
+			} else if (trackName.equalsIgnoreCase("Outpost Pass"))
+			{
+				trackSelection = "OutpostPass";
+				trackSelected = true;
+				break;
+			}
+			
+			System.out.println("Which track will we be racing today?");
+			System.out.println("[Crash Canyon] || [Crag City] || [Big Rock Island] || [Outpost Pass]");
+			
+		}
+		Track raceTrack = new Track(trackSelection);
 		
 		// Get number of racers from user input
-		System.out.println("How many racers?");
-		numRacers = Integer.parseInt(keyboard.nextLine());
+		while(numRacers < 1 || numRacers > 8)
+		{
+			System.out.println("How many racers? [1 - 8]");
+			numRacers = Integer.parseInt(keyboard.nextLine());
+		}
 		
 		// Get pilot file names from user input
+		ArrayList<String> remainingPilots = new ArrayList<String>(
+				Arrays.asList("Captain Phil", "Zanton", "Hazzel Frax",
+						"Bingstar Megadung", "Glim Sarwo", "Himmawon",
+						"Klarbshtabbel Vanderhoosht", "Yuteil Vin"));
+				
+		String pilotSelection = "";
 		String pilotName = "";
 		for (int index = 0; index < numRacers; ++index)
 		{
+			boolean pilotSelected = false;
 			System.out.println("Who is Pilot No. " + (index + 1) + "?");
-			pilotName = keyboard.nextLine();
-			pilots.add(new Pilot(pilotName));
+			remainingPilots.forEach(p -> System.out.println("| [" + p + "] |"));
+			
+			while(!pilotSelected)
+			{
+				pilotName = keyboard.nextLine();
+				if (pilotName.equalsIgnoreCase("Captain Phil"))
+				{
+					pilotSelection = "CaptainPhil";
+					pilotSelected = true;
+					remainingPilots.remove("Captain Phil");
+					break;
+				} else if (pilotName.equalsIgnoreCase("Zanton"))
+				{
+					pilotSelection = "Zanton";
+					pilotSelected = true;
+					remainingPilots.remove("Zanton");
+					break;
+				} else if (pilotName.equalsIgnoreCase("Hazzel Frax"))
+				{
+					pilotSelection = "HazzelFrax";
+					pilotSelected = true;
+					remainingPilots.remove("Hazzel Frax");
+					break;
+				} else if (pilotName.equalsIgnoreCase("Bingstar Megadung"))
+				{
+					pilotSelection = "BingstarMegadung";
+					pilotSelected = true;
+					remainingPilots.remove("Bingstar Megadung");
+					break;
+				} else if (pilotName.equalsIgnoreCase("Glim Sarwo"))
+				{
+					pilotSelection = "GlimSarwo";
+					pilotSelected = true;
+					remainingPilots.remove("Glim Sarwo");
+					break;
+				} else if (pilotName.equalsIgnoreCase("Himmawon"))
+				{
+					pilotSelection = "Himmawon";
+					pilotSelected = true;
+					remainingPilots.remove("Himmawon");
+					break;
+				} else if (pilotName.equalsIgnoreCase("Klarbshtabbel Vanderhoosht"))
+				{
+					pilotSelection = "KlarbshtabbelVanderhoosht";
+					pilotSelected = true;
+					remainingPilots.remove("Klarbshtabbel Vanderhoosht");
+					break;
+				} else if (pilotName.equalsIgnoreCase("Yuteil Vin"))
+				{
+					pilotSelection = "YuteilVin";
+					pilotSelected = true;
+					remainingPilots.remove("Yuteil Vin");
+					break;
+				}
+				
+				System.out.println("Who is Pilot No. " + (index + 1) + "?");
+				remainingPilots.forEach(p -> System.out.println("| [" + p + "] |"));
+				
+			}
+			pilots.add(new Pilot(pilotSelection));
+			
 		}
 		
 		//for (Pilot item : pilots) System.out.println("\t" + item.getWillPower());			// TEST
 		
 		// Get ship file names from user input
+		ArrayList<String> remainingShips = new ArrayList<String>(
+				Arrays.asList("SS Bhutan", "XZ32", "Space Hornet 7",
+						"Far Flyer", "Clipfang", "KG780",
+						"Shilber", "Dogfood"));
+				
+		String shipSelection = "";
 		String shipName = "";
 		for (int index = 0; index < numRacers; ++index)
 		{
+			boolean shipSelected = false;
 			System.out.println("Which ship will Pilot No. " + (index + 1) + " fly?");
-			shipName = keyboard.nextLine();
-			pilots.get(index).readShip(shipName);
+			remainingShips.forEach(p -> System.out.println("| [" + p + "] |"));
+			
+			while (!shipSelected)
+			{
+				shipName = keyboard.nextLine();
+				
+				if (shipName.equalsIgnoreCase("SS Bhutan"))
+				{
+					shipSelection = "SSBhutan";
+					shipSelected = true;
+					remainingShips.remove("SS Bhutan");
+					break;
+				} else if (shipName.equalsIgnoreCase("XZ32"))
+				{
+					shipSelection = "XZ32";
+					shipSelected = true;
+					remainingShips.remove("XZ32");
+					break;
+				} else if (shipName.equalsIgnoreCase("Space Hornet 7"))
+				{
+					shipSelection = "SpaceHornet7";
+					shipSelected = true;
+					remainingShips.remove("Space Hornet 7");
+					break;
+				} else if (shipName.equalsIgnoreCase("Far Flyer"))
+				{
+					shipSelection = "FarFlyer";
+					shipSelected = true;
+					remainingShips.remove("Far Flyer");
+					break;
+				} else if (shipName.equalsIgnoreCase("Clipfang"))
+				{
+					shipSelection = "Clipfang";
+					shipSelected = true;
+					remainingShips.remove("Clipfang");
+					break;
+				} else if (shipName.equalsIgnoreCase("KG780"))
+				{
+					shipSelection = "KG780";
+					shipSelected = true;
+					remainingShips.remove("KG780");
+					break;
+				} else if (shipName.equalsIgnoreCase("Shilber"))
+				{
+					shipSelection = "Shilber";
+					shipSelected = true;
+					remainingShips.remove("Shilber");
+					break;
+				} else if (shipName.equalsIgnoreCase("Dogfood"))
+				{
+					shipSelection = "Dogfood";
+					shipSelected = true;
+					remainingShips.remove("Dogfood");
+					break;
+				}
+			}
+			pilots.get(index).readShip(shipSelection);
 		}
 		
 		// Get number of mines from user input
